dd746ed4d20d9d72e604fd91d3d5e812a9aaad43
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2026-02-25 23:15:04 +0600

    class 10 js execution

---
+++ app.js
@@ -0,0 +1,10 @@
+let _q = 5;
+let y = 6;
+
+let first_name = "Tushar";
+let FirstName = "2nd Tushar";
+let firstName = "3rd Tushar";
+
+// let z = x + y
+
+console.log(first_name);
---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello, MERN Stack 1!");
+console.log(2 + 2);
+console.log("This is index.js file");
