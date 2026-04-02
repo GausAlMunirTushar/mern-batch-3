951a3ab774ba8642a9ef652ae52811024937f8f1
Gaus Al Munir Tushar <gausalmunirtushar@gmail.com>
2026-04-02 12:56:44 +0600

    js class 1

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-console.log("Hello, MERN Stack 1!");
-console.log(2 + 2);
-console.log("This is index.js file");
+const bangladesh = {
+	population: 2343434,
+	totalCity: 3434,
+	capital: "dhaka",
+	currency: "taka",
+	dollarRate: 122,
+};
+let age = 344;
+let phoneNumber = ["111111111111111111", "2222222222222", "3333333333"];
+
+console.log(phoneNumber[2]);
+
+console.log(age);
+
+// var address = "dhaka"
+// var address = "rangpur"
+// address = "khulna"
+
+const address = "dhaka";
+address = "rangpur";
+
+console.log(address);
